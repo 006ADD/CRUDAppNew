2265e2f64c49c264fb1f614dbd4850ef0596dbfe
006ADD <98921033+006ADD@users.noreply.github.com>
2023-12-20 00:28:11 +0600

    Add files via upload

---
+++ CRUDApp1/src/main/java/kg/add/springcourse/models/Person.java
@@ -0,0 +1,73 @@
+package kg.add.springcourse.models;
+
+import jakarta.validation.constraints.*;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name should not empty")
+    @Size(min=2, max=30, message="Name should be between 2 and characters")
+    private String name;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Min(value = 0, message = "Age should be greater than 0")
+    private int age;
+
+    @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{6}", message = "Your address should be in format: Country, City, Postal Code(6 digit)")
+    private String address;
+
+    public Person(){
+
+    }
+
+    public Person(int id, String name, int age, String email, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
